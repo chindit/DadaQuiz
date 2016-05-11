140cbd7be579a545dc0ad386322f3af9a8e73f87
chindit <littletiger58@gmail.com>
2016-05-11 14:04:04 +0200

    User answer displayed for Order type question

--- src/controllers/quiz.php
+++ src/controllers/quiz.php
@@ -11,17 +11,20 @@ if(!is_numeric($_GET['quiz'])){
     header('Location:index.php?page=404');
 }
 
-$valided = false;
 $data = array();
 $twigData = array();
+$twigData['valided'] = false;
 
 $quizManager = new QuizManager();
 $quiz = $quizManager->getQuiz($_GET['quiz']);
 
 //Checking history
-$isHistory = false;
+$twigData['isHistory'] = false;
 if(isset($_GET['history']) && is_numeric($_GET['history'])){
+    //History is asked -> retreiving data
     $historyData = $quizManager->getHistory($_GET['history'], $_GET['quiz']);
+    
+    //No data -> Exception
     if($historyData === FALSE){
         throw new Exception('L\'historique demandé n\'existe pas!');
     }
@@ -30,18 +33,22 @@ if(isset($_GET['history']) && is_numeric($_GET['history'])){
         throw new ErrorException('Cet historique ne vous appartient pas!  Galopin va!');
     }
     //Everything is OK
-    $valided = true;
-    $isHistory = true;
+    $twigData['valided'] = true; //Showing results
+    $twigData['isHistory'] = true;
     
     //Merging data
     $twigData = array_merge($twigData, $historyData);
 }
 
-if(isset($_POST['submit']) && !$valided){
+//Quiz submitted
+if(isset($_POST['submit']) && !$twigData['valided']){
+    //Checking validity for every answer
     foreach($_POST as $key => $val){
+        //If array() -> checkbox
         if(is_array($val)){
-            //Checkbox!
             $subAnswers = array();
+            
+            //Need numerical value
             foreach($val as $subKey => $subVal){
                 if(is_numeric($subVal)){
                     $subAnswers[] = $subVal;
@@ -49,13 +56,16 @@ if(isset($_POST['submit']) && !$valided){
             }
             $data[$key] = $subAnswers;
         }
+        //If $key and $val are numeric -> radio
         if(is_numeric($key) && is_numeric($val)){
             //Radio
             $data[$key] = $val;
         }
+        //If only one is numeric -> Order
         if(is_numeric($key) && !is_numeric($val)){
             //We check if it's an «Order» question
             $questions = $quiz->getQuestions();
+            //We need to parse all questions to find the selected one :(
             foreach($questions as $quest){
                 if($quest->getId() == $key && $quest->getType() == 'order'){
                     //It's an order-> checking answer
@@ -64,29 +74,29 @@ if(isset($_POST['submit']) && !$valided){
                     //Putting correction into var
                     $data[$key]['user'] = $val;
                     $data[$key]['answer'] = $answer;
+                    $data[$key]['array'] = explode(',', $val);
                     
                     break; //End of loop
                 }
             }
         }
     }
-    $valided = true; //Quiz submitted
+    $twigData['valided'] = true; //Quiz submitted
     
     //STATS
     $data['points'] = $quizManager->getPoints($quiz, $data);
-    var_dump($data);
+
     //Saving scores
     $quizManager->saveHistory($data, $quiz); //$_GET is trusted.  See L19.
     
     $twigData['data'] = $data;
 }
-
+var_dump($twigData['data']);
 //Counting nb of records in history
 $nbHistory = $quizManager->countHistory($_GET['quiz']);
 
+//Setting last data
 $twigData['quiz'] = $quiz;
-$twigData['valided'] = $valided;
-$twigData['isHistory'] = $isHistory;
 $twigData['nbHistory'] = $nbHistory;
 
 //Rendering
